f57f1743e677f9597357a506596080aaaf5379e0
ipa-ych <st179367@stud.uni-stuttgart.de>
2024-04-03 16:31:27 +0200

    add tricycle_steering_controller

---
+++ src/cmd_vel_bridge_node.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist, TwistStamped
+from std_msgs.msg import Header
+
+class CmdVelBridge(Node):
+    def __init__(self):
+        super().__init__('cmd_vel_bridge')
+        self.subscription = self.create_subscription(
+            Twist,
+            'cmd_vel_out',
+            self.cmd_vel_callback,
+            10
+        )
+        # self.publisher = self.create_publisher(TwistStamped, 'cmd_vel_stamped', 10)
+        self.publisher = self.create_publisher(TwistStamped, 'tricycle_steering_controller/reference', 10)
+        
+    def cmd_vel_callback(self, msg):
+        twist_stamped_msg = TwistStamped()
+        twist_stamped_msg.header.stamp = self.get_clock().now().to_msg()
+        twist_stamped_msg.header.frame_id = 'base_link'  # Set your desired frame id
+        twist_stamped_msg.twist = msg
+        self.publisher.publish(twist_stamped_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    cmd_vel_bridge = CmdVelBridge()
+    rclpy.spin(cmd_vel_bridge)
+    cmd_vel_bridge.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
