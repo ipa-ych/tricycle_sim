982bbcb1d4955a7560bed90a41e4546058d7ee4a
ipa-ych <st179367@stud.uni-stuttgart.de>
2024-04-08 22:56:59 +0200

    add base_footprint_joint for drive_kinematic

---
+++ launch/tricycle_drive_nav2_0408.launch.py
@@ -0,0 +1,153 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.substitutions import LaunchConfiguration
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+             )
+
+    tricycle_sim_path = os.path.join(
+        get_package_share_directory('tricycle_sim'))
+
+    xacro_file = os.path.join(tricycle_sim_path,
+                              'urdf',
+                              'test_tricycle_drive.xacro.urdf')
+
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'tricycle'],
+                        output='screen')
+
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_tricycle_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'tricycle_controller'],
+        output='screen'
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory('tricycle_sim'),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/tricycle_controller/cmd_vel')]
+        )
+    
+    teleop = Node(
+        package="teleop_twist_keyboard",
+        executable="teleop_twist_keyboard",
+        # parameters=[{'stamped': True, 'frame_id': 'base_link'}],
+        prefix = 'xterm -e',
+        output='screen',
+        remappings=[('/cmd_vel','/tricycle_controller/cmd_vel')]
+    )
+    
+    tricycle_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["tricycle_controller"],
+    )
+
+    cmd_vel_bridge_Twist = Node(
+        package="tricycle_sim",
+        executable="cmd_vel_bridge_Twist_node.py",
+        output='screen'
+    )
+
+
+    teleop_twist_joy_dir = get_package_share_directory('teleop_twist_joy')
+    joy_config = LaunchConfiguration('joy_config', default='xbox')
+
+    teleop_joy = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(teleop_twist_joy_dir, 'launch', 'teleop-launch.py')
+            ),
+            launch_arguments={'joy_config': joy_config}.items()
+        )
+
+    nav2_dir = get_package_share_directory('nav2_bringup')
+    nav2_config = os.path.join(get_package_share_directory('tricycle_sim'),'config','navi_tricycle_drive.yaml')
+    nav2_node = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(nav2_dir, 'launch', 'navigation_launch.py')
+            ),
+            launch_arguments={'params_file': '--ros-args --params-file ' + nav2_config}.items()
+        )
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        # arguments=[
+        #     '-d',
+        #     os.path.join(tricycle_sim_path, 'config/config.rviz'),
+        # ],
+        output='screen',
+    )
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_tricycle_controller],
+            )
+        ),
+        gazebo,
+        rviz,
+        node_robot_state_publisher,
+        spawn_entity,
+        twist_mux,
+        cmd_vel_bridge_Twist,
+        teleop_joy,
+        # nav2_node,
+        # tricycle_drive_spawner
+    ])
